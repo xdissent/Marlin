b6328548ad2a20670c4588635e5f22cacb6e1a5c
Hans Raaf <hara@oderwat.de>
2017-05-14 13:57:47 +0200

    Added a better version string schema.

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -35,7 +35,7 @@
   /**
    * Marlin release version identifier
    */
-  #define SHORT_BUILD_VERSION "SkyNet3D-1.1.0-1"
+  #define SHORT_BUILD_VERSION "Marlin 1.1.0-1"
 
   /**
    * Verbose version identifier which should contain a reference to the location
