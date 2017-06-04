ec7f1bdd717ae4ae4fc7bb07ae44cf09cc866d40
Greg Thornton <xdissent@me.com>
2017-06-04 04:15:11 -0500

    SkyNet3D - Anet Printer changes (with no sensor config)
    Changed Anet button detection ranges and resolution.
    
    Updated ANet 5 Button LCD implementaiton to newer Marlin
    
    Improved 5 button display (value editing with acceleration)
    
    Right button now either resets values to 0 or returns to status menu
    
    Changes to support RepRap Discount Full Graphics Display:
    See http://www.thingiverse.com/thing:2103748 for wiring!
    
    BLTouch / 3D-Touch Pins for Anet A8/A6 Board and Displays:
    - Pin 27 (wire 3 counting from red) for 5 Button and RepRap Discount
    - Pin 29 (wire 7 counting from red) for A6 Displays

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -35,7 +35,7 @@
   /**
    * Marlin release version identifier
    */
-  #define SHORT_BUILD_VERSION "1.1.1"
+  #define SHORT_BUILD_VERSION "SkyNet3D-1.1.0-1"
 
   /**
    * Verbose version identifier which should contain a reference to the location
@@ -70,7 +70,7 @@
   /**
    * Defines a generic printer name to be output to the LCD after booting Marlin.
    */
-  #define MACHINE_NAME "3D Printer"
+  #define MACHINE_NAME "SkyNet3D"
 
   /**
    * The SOURCE_CODE_URL is the location where users will find the Marlin Source
